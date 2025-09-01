6252429bd70fbc05fe5e0bd985bdfd393b05b9be
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-09-02 00:33:15 +0800

    .

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 source /lustre/fsw/portfolios/av/users/shiyil/anaconda3/etc/profile.d/conda.sh
 conda activate self_forcing
 git pull origin main
-torchrun --nnodes=1 --nproc_per_node=7 \
+torchrun --nnodes=1 --nproc_per_node=6 \
   train.py \
   --config_path configs/self_forcing_dmd.yaml \
   --logdir logs/self_forcing_dmd \
